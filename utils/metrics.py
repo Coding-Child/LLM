aac8dfa582621ba4adfc06798ce86ed7dbe84fda
Suhyeong <pshpulip40@gmail.com>
2024-04-28 17:33:33 +0900

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -4,11 +4,10 @@ from datasets import load_metric
 def compute_metrics(pred):
     labels = pred.label_ids
     preds = pred.predictions
+    preds = preds.argmax(-1)
 
     bleu = load_metric('bleu')
-    rouge = load_metric('rouge')
 
-    bleu_score = bleu.compute(predictions=preds, references=labels)
-    rouge_score = rouge.compute(predictions=preds, references=labels)
-
-    return {'eval_bleu': bleu_score, 'eval_rouge': rouge_score}
+    bleu_score = bleu.compute(predictions=preds, references=labels, trust_remote_code=True)
+    
+    return {'bleu': bleu_score['bleu']}
