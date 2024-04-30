b7e0c9d48867d6834df3e9b345584c0980e8d40d
Suhyeong <pshpulip40@gmail.com>
2024-04-30 23:51:28 +0900

    Update MedDialogueDataset.py

--- Dataset/MedDialogueDataset.py
+++ Dataset/MedDialogueDataset.py
@@ -1,29 +1,8 @@
-import json
-import torch
-from torch.utils.data import Dataset
-
-
-def preprocess_data(data_point, tokenizer, max_length=512):
+def preprocess_data(data_point, tokenizer):
     context = data_point['utterances'][0].split(":")[-1]
     response = data_point['utterances'][1].split(":")[-1]
 
-    contexts = tokenizer.encode_plus(context,
-                                     padding='max_length',
-                                     truncation=True,
-                                     max_length=max_length,
-                                     return_tensors='pt'
-                                     )
+    prompt = f'<Human>: {context}\n<Machine>: {response}'.strip()
+    tokenized_full_prompt = tokenizer(prompt, padding=True, truncation=True)
 
-    labels = tokenizer.encode_plus(response,
-                                   padding='max_length',
-                                   truncation=True,
-                                   max_length=max_length,
-                                   return_tensors='pt'
-                                   )['input_ids']
-
-    encoded = {'input_ids': contexts['input_ids'].squeeze(0),
-               'attention_mask': contexts['attention_mask'].squeeze(0),
-               'labels': labels.squeeze(0)
-               }
-
-    return encoded
+    return tokenized_full_prompt
