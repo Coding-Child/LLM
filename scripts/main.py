893fb92fff4a753d20ceb00aa2a00fbf0b831354
Suhyeong <pshpulip40@gmail.com>
2024-05-01 00:22:02 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -89,7 +89,7 @@ def main(args):
                                       remove_unused_columns=False,
                                       gradient_accumulation_steps=4,
                                       load_best_model_at_end=True,
-                                      metric_for_best_model='bleu',
+                                      metric_for_best_model='f1',
                                       greater_is_better=True,
                                       report_to="wandb"
                                       )
