4d1f8581c3a45a2d96280fbadb06c301b68bde22
Suhyeong <pshpulip40@gmail.com>
2024-05-01 23:51:20 +0900

    Update MedDialogueDataset.py

--- Dataset/MedDialogueDataset.py
+++ Dataset/MedDialogueDataset.py
@@ -1,3 +1,6 @@
+import torch
+
+
 def preprocess_data(data_point, tokenizer):
     context = data_point['utterances'][0].split(":")[-1]
     response = data_point['utterances'][1].split(":")[-1]
@@ -5,12 +8,18 @@ def preprocess_data(data_point, tokenizer):
     prompt = f'<Human>: {context}\n<Machine>: {response}'.strip()
     tokenized_full_prompt = tokenizer(prompt, padding=True, truncation=True)
 
-    labels = tokenized_full_prompt.input_ids.clone()
-    machine_token_index = tokenizer.encode('<Machine>:', add_special_tokens=False)
+    input_ids_list = tokenized_full_prompt.input_ids
+    labels = input_ids_list.copy()
 
-    response_start = tokenized_full_prompt.input_ids[0].tolist().index(machine_token_index[-1]) + 1
-    labels[0, :response_start] = -100
+    machine_token_id = 29076
+    machine_token_index = input_ids_list.index(machine_token_id)
 
-    return {'input_ids': tokenized_full_prompt.input_ids.flatten(),
-            'attention_mask': tokenized_full_prompt.attention_mask.flatten(),
-            'labels': labels.flatten()}
+    machine_end_index = machine_token_index + 2
+
+    labels[:machine_end_index] = [-100] * machine_end_index
+
+    return {
+        'input_ids': torch.tensor(input_ids_list),
+        'attention_mask': torch.tensor(tokenized_full_prompt.attention_mask),
+        'labels': torch.tensor(labels)
+    }
