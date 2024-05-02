830fe200042992d51c66dca36649b1831de0f981
Suhyeong <pshpulip40@gmail.com>
2024-05-02 12:52:52 +0900

    Update MedDialogueDataset.py

--- Dataset/MedDialogueDataset.py
+++ Dataset/MedDialogueDataset.py
@@ -3,8 +3,7 @@ def preprocess_data(data_point, tokenizer):
     response = data_point['utterances'][1].split(":")[-1]
 
     prompt = f'<Human>: {context}\n<Machine>: {response}'.strip()
-    tokenized_full_prompt = tokenizer(prompt, padding='max_length', max_length=1024, truncation=True, return_tensors='pt')
-
-    tokenized_full_prompt.labels = tokenized_full_prompt.input_ids.clone()
+    tokenized_full_prompt = tokenizer(prompt, padding=True, max_length=1024, truncation=True, return_tensors='pt')
+    tokenized_full_prompt['labels'] = tokenized_full_prompt['input_ids'].detach().clone()
 
     return tokenized_full_prompt
