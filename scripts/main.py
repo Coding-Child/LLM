8d32148a56d2e5885b660e9cc256d64385d8e88d
Suhyeong <pshpulip40@gmail.com>
2024-05-03 02:00:05 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -50,6 +50,8 @@ def main(args):
     model = LLM(r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout, model_name=model_name)
     model.llm.print_trainable_parameters()
 
+    torch.cuda.empty_cache()
+
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model, device_ids=[0, 1])
     model = model.to(device)
