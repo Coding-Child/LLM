18dc73f3d05970c02d3fc99094a75b21a4c3680f
Suhyeong <pshpulip40@gmail.com>
2024-05-03 01:27:19 +0900

    Update MedDialogueDataset.py

--- Dataset/MedDialogueDataset.py
+++ Dataset/MedDialogueDataset.py
@@ -1,11 +1,56 @@
-def preprocess_data(data_point, tokenizer):
-    context = data_point['utterances'][0].split(":")[-1]
-    response = data_point['utterances'][1].split(":")[-1]
+from torch.utils.data import Dataset
 
-    prompt = f'<Human>: {context}\n<Machine>: {response}'.strip()
-    tokenized_full_prompt = tokenizer(prompt, padding='max_length', max_length=1024, truncation=True, return_tensors='pt')
+import json
 
-    tokenized_full_prompt['input_ids'] = tokenized_full_prompt['input_ids'].flatten()
-    tokenized_full_prompt['attention_mask'] = tokenized_full_prompt['attention_mask'].flatten()
 
-    return tokenized_full_prompt
+class MedDialogueDataset(Dataset):
+    def __init__(self, tokenizer, dataset, max_length=512, stride=512):
+        self.tokenizer = tokenizer
+
+        with open(dataset, 'r') as f:
+            self.dataset = json.load(f)
+
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.stride = stride
+
+        self.inputs = []
+        self.labels = []
+
+        self._preprocess()
+
+    def _preprocess(self):
+        for d in self.dataset:
+            human_text = d["utterances"][0].split(":")[-1]
+            machine_text = d["utterances"][1].split(":")[-1]
+            concatenated_text = f'<Human>: {human_text}\n<Machine>: {machine_text}'
+
+            encodings = self.tokenizer(concatenated_text,
+                                       padding='max_length',
+                                       max_length=self.max_length,
+                                       truncation=True,
+                                       return_tensors="pt")
+
+            for i in range(0, encodings.input_ids.size(1), self.stride):
+                begin_loc = max(i + self.stride - self.max_length, 0)
+                end_loc = min(i + self.stride, encodings.input_ids.size(1))
+                trg_len = end_loc - i
+
+                input_ids = encodings.input_ids[:, begin_loc: end_loc]
+                attention_mask = encodings.attention_mask[:, begin_loc: end_loc]
+
+                target_ids = input_ids.clone()
+                target_ids[:, :-trg_len] = -100
+
+                self.inputs.append({'end_loc': end_loc, 'input_ids': input_ids.flatten(), 'attention_mask': attention_mask.flatten()})
+                self.labels.append(target_ids.flatten())
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, idx: int):
+        return {'end_loc': self.inputs[idx]['end_loc'],
+                'input_ids': self.inputs[idx]['input_ids'],
+                'attention_mask': self.inputs[idx]['attention_mask'],
+                'labels': self.labels[idx]
+                }
