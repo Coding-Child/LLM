d7083be20a3991b85c7f23142bf4d4e032780a42
Suhyeong <pshpulip40@gmail.com>
2024-05-03 17:30:42 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,24 +1,30 @@
 import os
 import random
 import numpy as np
-import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
-import torch.optim as opt
-from torch.utils.data import DataLoader
 
 from transformers import AutoTokenizer
 from transformers import DataCollatorForLanguageModeling
-from transformers import get_linear_schedule_with_warmup
+from transformers import Trainer, TrainingArguments
+from evaluate import load
 
 from model.LLM import LLM
 from Dataset.MedDialogueDataset import MedDialogueDataset
-from scripts.train import train
-from scripts.eval import test
 
 os.environ["WANDB_PROJECT"] = "llm_training"
 os.environ["WANDB_LOG_MODEL"] = "checkpoints"
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    criterion = nn.CrossEntropyLoss()
+
+    loss = criterion(logits.view(-1, logits.size(-1)), labels.view(-1))
+    perplexity = torch.exp(torch.tensor(loss)).item()
+
+    return {'loss': loss.item(), 'ppl': perplexity}
 
 
 def seed_everything(seed):
@@ -47,14 +53,8 @@ def main(args):
     lora_dropout = args.lora_dropout
     lora_alpha = args.lora_alpha
 
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model = LLM(r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout, model_name=model_name)
     model.llm.print_trainable_parameters()
-    torch.cuda.empty_cache()
-
-    if torch.cuda.device_count() > 1:
-        model = nn.DataParallel(model, device_ids=[0, 1])
-    model = model.to(device)
 
     try:
         tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -73,48 +73,31 @@ def main(args):
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False, return_tensors='pt')
 
-    train_loader = DataLoader(train_data,
-                              batch_size=batch_size,
-                              shuffle=True,
-                              collate_fn=data_collator,
-                              num_workers=4,
-                              pin_memory=True)
-    val_loader = DataLoader(val_data,
-                            batch_size=batch_size,
-                            shuffle=False,
-                            collate_fn=data_collator,
-                            num_workers=4,
-                            pin_memory=True)
-    test_loader = DataLoader(test_data,
-                             batch_size=batch_size,
-                             shuffle=False,
-                             collate_fn=data_collator,
-                             num_workers=4,
-                             pin_memory=True)
+    training_args = TrainingArguments(per_device_train_batch_size=batch_size,
+                                      per_device_eval_batch_size=batch_size,
+                                      evaluation_strategy='epoch',
+                                      logging_dir='./logs',
+                                      logging_steps=1000,
+                                      save_strategy='epoch',
+                                      save_total_limit=1,
+                                      learning_rate=lr,
+                                      num_train_epochs=num_epochs,
+                                      gradient_accumulation_steps=accumulation_step,
+                                      report_to='wandb',
+                                      seed=args.seed,
+                                      load_best_model_at_end=True,
+                                      metric_for_best_model='ppl',
+                                      greater_is_better=False,
+                                      output_dir=save_path
+                                      )
+    trainer = Trainer(model=model,
+                      args=training_args,
+                      data_collator=data_collator,
+                      train_dataset=train_data,
+                      eval_dataset=val_data
+                      )
 
-    print("Training set size:", len(train_data))
-    print("Validation set size:", len(val_data))
-    print("Test set size:", len(test_data))
-
-    warmup_step = (len(train_loader) * num_epochs) * 0.04
-    total_step = len(train_loader) * num_epochs
-
-    optimizer = opt.AdamW(model.parameters(), lr=lr)
-    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=total_step)
-
-    train_loss_arr, val_loss_arr = train(model=model,
-                                         device=device,
-                                         train_loader=train_loader,
-                                         val_loader=val_loader,
-                                         optimizer=optimizer,
-                                         scheduler=scheduler,
-                                         num_epochs=num_epochs,
-                                         accumulation_step=accumulation_step,
-                                         save_path=save_path)
-
-    plt.plot(train_loss_arr, label='train loss', marker='o', color='red')
-    plt.plot(val_loss_arr, label='validation loss', marker='o', color='blue')
-    plt.legend()
-    plt.savefig(f'{save_path}/loss_plot.png')
-
-    test(model, device, test_loader, save_path)
+    trainer.train()
+    model.llm.save_pretrained(save_path)
+    result = trainer.evaluate(test_data)
+    print(result)
