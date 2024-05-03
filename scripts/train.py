fdb791add1bd8df51277f9b9463e1538eccf5aac
Suhyeong <pshpulip40@gmail.com>
2024-05-03 14:12:23 +0900

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -63,9 +63,6 @@ def train(model, device, train_loader, val_loader, optimizer, scheduler, num_epo
                 pbar.update(1)
                 step += 1
 
-                del output, loss, batch
-                torch.cuda.empty_cache()
-
         f1_score = f1.compute(predictions=preds, references=labels, average='macro')['f1']
         ppl = torch.exp(torch.stack(nlls).mean())
 
@@ -96,6 +93,9 @@ def train(model, device, train_loader, val_loader, optimizer, scheduler, num_epo
                    'val_f1': val_f1,
                    "global_step": step
                    })
+
+        del preds, labels, nlls, avg_loss, val_loss, val_ppl, val_f1, f1_score, ppl, output, loss, batch
+        torch.cuda.empty_cache()
 
     model.save_adapter(save_path + '/last_model')
     model.save_full_model(save_path + '/last_model')
