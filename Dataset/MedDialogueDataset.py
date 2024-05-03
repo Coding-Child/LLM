a2ee611c3f73adc37ffd40731f21ff9fd6a52f95
Suhyeong <pshpulip40@gmail.com>
2024-05-03 18:53:36 +0900

    Update MedDialogueDataset.py

--- Dataset/MedDialogueDataset.py
+++ Dataset/MedDialogueDataset.py
@@ -1,44 +1,10 @@
-from torch.utils.data import Dataset
+def preprocess_data(data_point, tokenizer):
+    context = data_point['utterances'][0].split(":")[-1]
+    response = data_point['utterances'][1].split(":")[-1]
 
-import json
+    prompt = f'<Human>: {context}\n<Machine>: {response}'.strip()
+    tokenized_full_prompt = tokenizer(prompt, padding='max_length', max_length=512, truncation=True, return_tensors='pt')
+    tokenized_full_prompt['input_ids'] = tokenized_full_prompt['input_ids'].flatten()
+    tokenized_full_prompt['attention_mask'] = tokenized_full_prompt['attention_mask'].flatten()
 
-
-class MedDialogueDataset(Dataset):
-    def __init__(self, tokenizer, dataset, max_length=512, stride=512):
-        self.tokenizer = tokenizer
-
-        with open(dataset, 'r') as f:
-            self.dataset = json.load(f)
-
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-        self.stride = stride
-
-        self.inputs = []
-        self.labels = []
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx: int):
-        d = self.dataset[idx]
-        human_text = d["utterances"][0].split(":")[-1]
-        machine_text = d["utterances"][1].split(":")[-1]
-        concatenated_text = f'<Human>: {human_text}\n<Machine>: {machine_text}'
-
-        encodings = self.tokenizer(concatenated_text,
-                                   padding='max_length',
-                                   max_length=self.max_length,
-                                   truncation=True,
-                                   return_tensors="pt")
-
-        input_ids = encodings.input_ids[0]
-        attention_mask = encodings.attention_mask[0]
-
-        target_ids = input_ids.clone()
-        target_ids[:-self.stride] = -100
-
-        return {'input_ids': input_ids.flatten(),
-                'attention_mask': attention_mask.flatten(),
-                'labels': target_ids.flatten()
-                }
+    return tokenized_full_prompt
