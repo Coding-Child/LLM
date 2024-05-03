2f0d92aa615b81a5c239327c0604e84243d3d0fd
Suhyeong <pshpulip40@gmail.com>
2024-05-03 16:39:09 +0900

    Update MedDialogueDataset.py

--- Dataset/MedDialogueDataset.py
+++ Dataset/MedDialogueDataset.py
@@ -17,39 +17,28 @@ class MedDialogueDataset(Dataset):
         self.inputs = []
         self.labels = []
 
-        self._preprocess()
-
-    def _preprocess(self):
-        for d in self.dataset:
-            human_text = d["utterances"][0].split(":")[-1]
-            machine_text = d["utterances"][1].split(":")[-1]
-            concatenated_text = f'<Human>: {human_text}\n<Machine>: {machine_text}'
-
-            encodings = self.tokenizer(concatenated_text,
-                                       padding='max_length',
-                                       max_length=self.max_length,
-                                       truncation=True,
-                                       return_tensors="pt")
-
-            for i in range(0, encodings.input_ids.size(1), self.stride):
-                begin_loc = max(i + self.stride - self.max_length, 0)
-                end_loc = min(i + self.stride, encodings.input_ids.size(1))
-                trg_len = end_loc - i
-
-                input_ids = encodings.input_ids[:, begin_loc: end_loc]
-                attention_mask = encodings.attention_mask[:, begin_loc: end_loc]
-
-                target_ids = input_ids.clone()
-                target_ids[:, :-trg_len] = -100
-
-                self.inputs.append({'end_loc': end_loc, 'input_ids': input_ids.flatten(), 'attention_mask': attention_mask.flatten()})
-                self.labels.append(target_ids.flatten())
-
     def __len__(self):
         return len(self.inputs)
 
     def __getitem__(self, idx: int):
-        return {'input_ids': self.inputs[idx]['input_ids'],
-                'attention_mask': self.inputs[idx]['attention_mask'],
-                'labels': self.labels[idx]
+        d = self.dataset[idx]
+        human_text = d["utterances"][0].split(":")[-1]
+        machine_text = d["utterances"][1].split(":")[-1]
+        concatenated_text = f'<Human>: {human_text}\n<Machine>: {machine_text}'
+
+        encodings = self.tokenizer(concatenated_text,
+                                   padding='max_length',
+                                   max_length=self.max_length,
+                                   truncation=True,
+                                   return_tensors="pt")
+
+        input_ids = encodings.input_ids[0]
+        attention_mask = encodings.attention_mask[0]
+
+        target_ids = input_ids.clone()
+        target_ids[:-self.stride] = -100
+
+        return {'input_ids': input_ids.flatten(),
+                'attention_mask': attention_mask.flatten(),
+                'labels': target_ids.flatten()
                 }
